62f7e65b57521abb14ba04d82995165f725601c5
chenxz <563859095@qq.com>
2018-08-05 17:18:21 +0800

    commit

--- app/src/main/java/com/cxz/androidcustomview/activity/WaveViewActivity.java
+++ app/src/main/java/com/cxz/androidcustomview/activity/WaveViewActivity.java
@@ -9,6 +9,7 @@ import com.cxz.androidcustomview.widget.WaveBezierView;
 public class WaveViewActivity extends AppCompatActivity {
 
     private WaveBezierView wave_bezier_view;
+    private WaveBezierView wave_bezier_view2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +17,24 @@ public class WaveViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wave_view);
 
         wave_bezier_view = findViewById(R.id.wave_bezier_view);
+        wave_bezier_view2 = findViewById(R.id.wave_bezier_view2);
 
         wave_bezier_view.startWave();
+        wave_bezier_view2.startWave();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        wave_bezier_view.resumeWave();
+        wave_bezier_view2.resumeWave();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wave_bezier_view.pauseWave();
+        wave_bezier_view2.pauseWave();
     }
 
     @Override
@@ -25,5 +42,6 @@ public class WaveViewActivity extends AppCompatActivity {
         super.onDestroy();
 
         wave_bezier_view.stopWave();
+        wave_bezier_view2.stopWave();
     }
 }
