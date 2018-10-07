927a9403de825c1d8c67b13917876472e70a69ae
chenxz <563859095@qq.com>
2018-10-07 22:18:32 +0800

    commit

--- roll-textview/src/main/java/com/cxz/roll/textview/RollTextView.java
+++ roll-textview/src/main/java/com/cxz/roll/textview/RollTextView.java
@@ -1,9 +1,35 @@
 package com.cxz.roll.textview;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
 /**
  * @author chenxz
  * @date 2018/9/24
  * @desc
  */
-public class RollTextView {
+public class RollTextView extends RelativeLayout {
+    public RollTextView(Context context) {
+        this(context, null);
+    }
+
+    public RollTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+
+    public RollTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    /**
+     * 初始化
+     *
+     * @param context
+     * @param attrs
+     */
+    private void init(Context context, AttributeSet attrs) {
+
+    }
 }
