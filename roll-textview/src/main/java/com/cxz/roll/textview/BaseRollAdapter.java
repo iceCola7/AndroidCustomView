8ce33df508af7e9113f981e400b988fa3a045808
chenxz <563859095@qq.com>
2018-10-22 17:17:01 +0800

    commit

---
+++ roll-textview/src/main/java/com/cxz/roll/textview/BaseRollAdapter.java
@@ -0,0 +1,63 @@
+package com.cxz.roll.textview;
+
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author chenxz
+ * @date 2018/10/22
+ * @desc
+ */
+public abstract class BaseRollAdapter<T> {
+
+    private List<T> mDatas;
+    private OnDataChangedListener mOnDataChangedListener;
+
+    public BaseRollAdapter(List<T> datas) {
+        this.mDatas = datas;
+        if (datas == null || datas.isEmpty()) {
+            throw new RuntimeException("no data");
+        }
+    }
+
+    public BaseRollAdapter(T[] datas) {
+        mDatas = new ArrayList<>(Arrays.asList(datas));
+    }
+
+    public void setDatas(List<T> datas) {
+        this.mDatas = datas;
+    }
+
+    void setOnDataChangedListener(OnDataChangedListener listener) {
+        mOnDataChangedListener = listener;
+    }
+
+    public int getCount() {
+        return mDatas == null ? 0 : mDatas.size();
+    }
+
+    public T getItem(int position) {
+        return mDatas.get(position);
+    }
+
+    void notifyDataChanged() {
+         mOnDataChangedListener.onChanged();
+    }
+
+    /**
+     * 设置 RollTextView 的样式
+     */
+    public abstract View getView(RollTextView parent);
+
+    /**
+     * 设置 RollTextView 的数据
+     */
+    public abstract void setItem(View view, T data);
+
+    interface OnDataChangedListener {
+        void onChanged();
+    }
+}
--- roll-textview/src/main/java/com/cxz/roll/textview/RollTextView.java
+++ roll-textview/src/main/java/com/cxz/roll/textview/RollTextView.java
@@ -1,15 +1,41 @@
 package com.cxz.roll.textview;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.widget.RelativeLayout;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * @author chenxz
  * @date 2018/9/24
  * @desc
  */
-public class RollTextView extends RelativeLayout {
+public class RollTextView extends LinearLayout implements BaseRollAdapter.OnDataChangedListener {
+
+    private float mRollTVHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+
+    private int mGap = 4000;
+    private int mAnimDuration = 1000;
+    private BaseRollAdapter mAdapter;
+    private Paint mPaint;
+
+    private View mFirstView;
+    private View mSecondView;
+
+    private int mPosition;
+    private boolean isStarted;
+    private AnimRunnable mRunnable = new AnimRunnable();
+
     public RollTextView(Context context) {
         this(context, null);
     }
@@ -30,6 +56,151 @@ public class RollTextView extends RelativeLayout {
      * @param attrs
      */
     private void init(Context context, AttributeSet attrs) {
+        setOrientation(VERTICAL);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.RollTextView);
+        mGap = array.getInteger(R.styleable.RollTextView_gap, mGap);
+        mAnimDuration = array.getInteger(R.styleable.RollTextView_anim_duration, mAnimDuration);
 
+        if (mGap <= mAnimDuration) {
+            mGap = 4000;
+            mAnimDuration = 1000;
+        }
+        array.recycle();
+    }
+
+    /**
+     * 设置适配器
+     */
+    public void setAdapter(BaseRollAdapter adapter) {
+        if (adapter == null) {
+            throw new RuntimeException("adapter must be not null");
+        }
+        if (mAdapter != null) {
+            throw new RuntimeException("you have already set an adapter");
+        }
+        this.mAdapter = adapter;
+        mAdapter.notifyDataChanged();
+        mAdapter.setOnDataChangedListener(this);
+        setupAdapter();
+    }
+
+    private void setupAdapter() {
+        removeAllViews();
+        if (mAdapter.getCount() == 1) {
+            mFirstView = mAdapter.getView(this);
+            mAdapter.setItem(mFirstView, mAdapter.getItem(0));
+            addView(mFirstView);
+        } else {
+            mFirstView = mAdapter.getView(this);
+            mSecondView = mAdapter.getView(this);
+            mAdapter.setItem(mFirstView, mAdapter.getItem(0));
+            mAdapter.setItem(mSecondView, mAdapter.getItem(1));
+            addView(mFirstView);
+            addView(mSecondView);
+
+            mPosition = 1;
+            isStarted = false;
+        }
+        setBackgroundDrawable(mFirstView.getBackground());
+    }
+
+    /**
+     * 开始
+     */
+    public void start() {
+        if (mAdapter == null) {
+            throw new RuntimeException("you must call setAdapter() before start");
+        }
+        if (!isStarted && mAdapter.getCount() > 1) {
+            isStarted = true;
+            postDelayed(mRunnable, mGap);
+        }
+    }
+
+    /**
+     * 结束
+     */
+    public void stop() {
+        removeCallbacks(mRunnable);
+        isStarted = false;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (LayoutParams.WRAP_CONTENT == heightMeasureSpec) {
+            getLayoutParams().height = (int) mRollTVHeight;
+        } else {
+            mRollTVHeight = getHeight();
+        }
+        if (isInEditMode()) {
+            setBackgroundColor(Color.GRAY);
+            return;
+        }
+        if (mFirstView != null) {
+            mFirstView.getLayoutParams().height = (int) mRollTVHeight;
+        }
+        if (mSecondView != null) {
+            mSecondView.getLayoutParams().height = (int) mRollTVHeight;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (isInEditMode()) {
+            mPaint.setColor(Color.WHITE);
+            mPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+            mPaint.setStyle(Paint.Style.STROKE);
+            canvas.drawText("RollTextView", 20, getHeight() * 2 / 3, mPaint);
+        }
+    }
+
+    private void performSwitch() {
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(mFirstView, "translationY", mFirstView.getTranslationY() - mRollTVHeight);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mSecondView, "translationY", mSecondView.getTranslationY() - mRollTVHeight);
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(animator1, animator2);
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mFirstView.setTranslationY(0);
+                mSecondView.setTranslationY(0);
+                View removedView = getChildAt(0);
+                mPosition++;
+                mAdapter.setItem(removedView, mAdapter.getItem(mPosition % mAdapter.getCount()));
+                removeView(removedView);
+                addView(removedView, 1);
+            }
+
+        });
+        set.setDuration(mAnimDuration);
+        set.start();
+    }
+
+    @Override
+    public void onChanged() {
+        setupAdapter();
+    }
+
+    class AnimRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            performSwitch();
+            postDelayed(this, mGap);
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        stop();
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
 }
